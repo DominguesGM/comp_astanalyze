ffd4cce13dd3de48179413dc2eca003f35596aa5
Gil Domingues <gmdz95@gmail.com>
2016-04-08 10:10:44 +0100

    added json parsing library json-simple. noticed an issue with the validity of spool2ast generated json

---
+++ src/test/JsonInput.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class JsonInput {
+	
+	public static void main(String args[]){
+		FileReader freader = null;
+		try {
+			freader = new FileReader("json\\ast.json");
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+		int read = 0;
+		String jsonString = "";
+		try {
+			while((read = freader.read()) != -1){
+				jsonString += (char) read;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		JSONObject json = new JSONObject();
+		System.out.println(json);
+		json = (JSONObject) JSONValue.parse(jsonString);
+		System.out.println(json);
+	}
+	
+}
