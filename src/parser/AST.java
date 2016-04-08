ae67e2d6225ce5f524bd08cea7dee521b8664bfc
Gil Domingues <gmdz95@gmail.com>
2016-04-08 10:30:09 +0100

    Added parse function to AST

--- src/parser/AST.java
+++ src/parser/AST.java
@@ -1,5 +1,30 @@
 package parser;
 
-public class AST {
+import java.io.FileReader;
+import java.io.IOException;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+public class AST {
+	private JSONObject ast;
+	
+	/**
+	 * parseFile.
+	 * Provided a file name of a json text file, generates the JSONObject.
+	 * @param astFile
+	 * @throws IOException
+	 */
+	public void parseFile(String astFile) throws IOException{
+		FileReader freader = null;
+		freader = new FileReader(astFile);
+		int read = 0;
+		String jsonString = "";
+		while((read = freader.read()) != -1){
+			jsonString += (char) read;
+		}
+		ast = (JSONObject) JSONValue.parse(jsonString);
+	}
+	
+	
 }
